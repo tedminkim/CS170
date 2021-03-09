c892a278a8814889af6d1bc00fcb08467b9b63c6
Ted Kim <ted.s.kim1004@gmail.com>
2021-03-08 23:48:05 -0800

    Slight edits, comments

--- Project2/project2.py
+++ Project2/project2.py
@@ -1,21 +1,23 @@
-import numpy as np 
-import pandas as pd 
 import csv 
-import random 
 import math 
 from os import path
 import os.path 
 from array import *
 
-def featureSearch(data):
-    currentSetOfFeatures = []
-    for i in range(len(data, 2) - 1):
+#def featureSearch(data):
+    #currentSetOfFeatures = []
+    #for i in range(len(data, 2) - 1):
         
 
 def main():
     userInput = ""
     print("Welcome to Ted Kim's Feature Selection Algorithm!")
     fileInput = input("Type in the name of the file to test: ")
+    while not path.exists(fileInput):
+        print("CRINGE!! THAT FILE ISN'T REAL.")
+        fileInput = input("Type in the name of the file to test:")
+
+
     print("Wow! Thanks for inputting the file name: " + fileInput + "! We'll doing something about it.")
     while userInput != '1' and userInput != '2':
         print("Type the number of the algorithm you want to run.")
